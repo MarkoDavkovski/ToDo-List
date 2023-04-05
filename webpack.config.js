d795f3cb29e7d10a06e329bc39e119b5e0d902e9
MarkoDavkovski <marko.davkovski@gmail.com>
2023-04-05 11:57:17 +0200

    Folder structure for the project.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: '[name][ext]',
+	},
+	devtool: 'source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: '[name][ext]',
+				},
+			},
+		],
+	},
+};
